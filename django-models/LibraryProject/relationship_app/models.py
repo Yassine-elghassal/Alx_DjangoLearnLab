24b52a1e876fe70c51d35b162e27279bbe94236e
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-03-22 12:34:27 +0000

    first commit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,30 +1,35 @@
+# relationship_app/models.py
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
+# Author model: This will store author names
 class Author(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+# Book model: This will store book titles, linked to an author through ForeignKey
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.title
 
+
+# Library model: This will store library names and their associated books (ManyToMany relationship)
 class Library(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
 
+
+# Librarian model: This will store librarian names, each assigned to a specific library (OneToOne relationship)
 class Librarian(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
