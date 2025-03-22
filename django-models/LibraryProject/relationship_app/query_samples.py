9ca356c9d5f721e5764997485f5f0ab02ba1d585
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-03-22 12:22:40 +0000

    first commit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,30 +1,30 @@
-import os
-import django
+# models.py
+from django.db import models
 
-# Set the settings module before calling django.setup()
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
-django.setup()
+    def __str__(self):
+        return self.name
 
-from relationship_app.models import Author, Book, Library, Librarian
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey('Library', on_delete=models.CASCADE)
 
-# Query all books by a specific author
-def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)
+    def __str__(self):
+        return self.title
 
-# List all books in a library
-def books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
 
-# Retrieve the librarian for a library
-def librarian_of_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    def __str__(self):
+        return self.name
 
-# Example Usage
-if __name__ == "__main__":
-    print(list(books_by_author("J.K. Rowling")))
-    print(list(books_in_library("Central Library")))
-    print(librarian_of_library("Central Library"))
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
