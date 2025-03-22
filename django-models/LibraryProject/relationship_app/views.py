bceb4bd0a02565fee827d549fa785193789455ff
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-03-22 12:46:44 +0000

    first commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,3 +31,15 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'  # This will be used in the template
+
+
+# relationship_app/views.py
+from django.shortcuts import render
+from django.views.generic.detail import DetailView  # Correct import for DetailView
+from .models import Library  # Import Library model
+
+# Class-based view to display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # This will be used in the template
