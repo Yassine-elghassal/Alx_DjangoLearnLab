070df4f7e51b462a5febf2496213af6152495a6b
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-03-22 12:45:21 +0000

    first commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 # relationship_app/views.py
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library  # This line imports the Library model
+
+# Class-based view to display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # This will be used in the template
+
+# relationship_app/views.py
+from django.shortcuts import render
 from relationship_app.models import Book
 
 def list_books(request):
