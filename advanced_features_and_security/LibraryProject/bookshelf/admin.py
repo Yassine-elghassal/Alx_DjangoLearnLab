216c8a32b4b20480f47986bbe66aad0fb328c84d
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-03-23 15:04:46 +0000

    first commit

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -21,3 +21,25 @@ class CustomUserAdmin(UserAdmin):
 # Register the custom user admin
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(LogEntry)
+
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    
+# Create the groups and assign permissions manually through the admin interface or use a data migration
+def create_groups():
+    # Admin group
+    admin_group, created = Group.objects.get_or_create(name='Admins')
+    admin_group.permissions.add(*Book._meta.permissions)  # Assign all permissions
+
+    # Editors group
+    editors_group, created = Group.objects.get_or_create(name='Editors')
+    editors_group.permissions.add('can_create', 'can_edit')
+
+    # Viewers group
+    viewers_group, created = Group.objects.get_or_create(name='Viewers')
+    viewers_group.permissions.add('can_view')
