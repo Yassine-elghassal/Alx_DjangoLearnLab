5a1299d416adedf1b3a160b3e74c2a64387569ba
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-03-23 12:56:45 +0000

    first commit

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -61,49 +61,15 @@ class Book(models.Model):
         ]
     
     def __str__(self):
-        return self.title
 
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.conf import settings
 
-# Custom User Manager
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        return self.create_user(email, password, **extra_fields)
-
-# Custom User Model
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
-    
-    username = None  # We will use email instead of username
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     def __str__(self):
-        return self.email
+        return self.username
 
-# Other Models using CustomUser
-class Librarian(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    hired_date = models.DateField()
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
