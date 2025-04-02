2a070ebe69ac69e70b3df20aaafea7fbdb8564b0
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-04-02 15:00:01 +0000

    first commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,13 +12,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
+        token = Token.objects.create(user=user)  # Explicitly creating a token
         return user
 
 
