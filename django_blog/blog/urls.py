12196dce74f24bc3eaee7d7cc156602561911c94
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-04-02 20:32:24 +0000

    first commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -38,7 +38,10 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:post_id>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
+
+    # Corrected URL for adding a comment
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
